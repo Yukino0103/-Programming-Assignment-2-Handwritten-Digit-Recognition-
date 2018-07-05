1e5d4c935dc4b2fc0c4c740da1925fb6305ed6c6
Yukino0103 <410421254@gms.ndhu.edu.tw>
2018-07-05 23:39:26 +0800

    Create MNIST_MLPHW2.py

---
+++ MNIST_MLPHW2.py
@@ -0,0 +1,51 @@
+from __future__ import print_function
+
+import keras
+from keras.datasets import mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+#x_train, x_test: uint8 数组表示的灰度图像，尺寸为 (num_samples, 28, 28)。
+#y_train, y_test: uint8 数组表示的数字标签（范围在 0-9 之间的整数），尺寸为 (num_samples,)。
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.optimizers import RMSprop
+#建立模型
+
+batch_size = 128
+num_classes = 10
+epochs = 20
+
+x_train = x_train.reshape(60000, 784)
+x_test = x_test.reshape(10000, 784)
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+#preprocessing character images
+
+
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+# convert class vectors to binary class matrices
+
+model = Sequential()
+model.add(Dense(512, activation='relu', input_shape=(784,)))
+model.add(Dropout(0.2))
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.summary()
+
+model.compile(loss='categorical_crossentropy',
+              optimizer=RMSprop(),
+              metrics=['accuracy'])
+
+history = model.fit(x_train, y_train,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    verbose=1,
+                    validation_data=(x_test, y_test))
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
